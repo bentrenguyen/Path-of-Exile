302b842cd3e17476e9c6985e4cad780a0af8aeca
Bentre Nguyen <bentre.nguyen@berkeley.edu>
2020-12-22 00:24:12 -0800

    labmap done

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,27 +80,33 @@ $(document).ready(function() {
 	  var act = $(this).val();
 	  $(".actsDiv").not(act).hide();
 	  $(act).show();
-	  /*
-	  if (act == "#act1") {
-		$(".actsDiv").not("#act1").hide();
-		$("#act1").show();
-	  } else if (act == "#act2") {
-		$(".actsDiv").not("#act2").hide();
-		$("#act2").show();
-	  } else if (act == "blue") {
-		$(".box").not(".blue").hide();
-		$(".blue").show();
-	  } else if (act == "maroon") {
-		$(".box").not(".maroon").hide();
-		$(".maroon").show();
-	  } else if (act == "magenta") {
-		$(".box").not(".magenta").hide();
-		$(".magenta").show();
-	  } else {
-		$(".box").hide();
-	  }
-	  */
-	});
-  
-  
+	});  
   });
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(document).ready(function() {
+	check();
+});
+
+function check() {
+	$("#contentDiv").load('generic.html #notesImg');
+}
