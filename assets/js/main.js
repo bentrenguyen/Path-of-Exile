f0e314dc02fcf989c158a4891cf63e50b3ba185d
Bentre Nguyen <bentre.nguyen@berkeley.edu>
2020-12-31 23:00:34 -0800

    back to top button fade

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,14 +96,21 @@ mybutton = document.getElementById("myBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+	if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+		$('#myBtn').fadeIn(speed = 'fast');
+	
+  	} else {
+		$('#myBtn').fadeOut(speed = 'fast');
+  	}
 }
 
+
+window.addEventListener('scroll', () => {
+	document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  }, false);
+  
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
