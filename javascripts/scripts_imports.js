47201ef6a5d4a19846ba2d49d289d8e1a378886e
bentrenguyen <bentre.nguyen@gmail.com>
2022-10-06 16:18:31 -0700

    adapted flasks to main format

---
+++ javascripts/scripts_imports.js
@@ -0,0 +1,20 @@
+import {copy} from "./scripts.js";
+window.copy = copy;
+
+import {reset} from "./scripts.js";
+window.reset = reset;
+
+import {searchbox} from "./scripts.js";
+window.searchbox = searchbox;
+
+import {change_display} from "./scripts.js";
+window.change_display = change_display;
+
+import {select_dropdown} from "./scripts.js";
+window.select_dropdown = select_dropdown;
+
+import {update_length} from "./scripts.js";
+window.update_length = update_length;
+
+import {searchboxflasks} from "./scripts.js";
+window.searchboxflasks = searchboxflasks;
